dda77f9216cf795321cde36906f815e1e9a4111e
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-10-28 01:31:43 +0530

    Update 2022. Convert 1D Array Into 2D Array Modulo.cpp

--- Biweekly Contest 62/2022. Convert 1D Array Into 2D Array Modulo.cpp
+++ Biweekly Contest 62/2022. Convert 1D Array Into 2D Array Modulo.cpp
@@ -1,8 +1,6 @@
-Question Link- https://leetcode.com/contest/biweekly-contest-62/problems/convert-1d-array-into-2d-array/
+[Question Link](https://leetcode.com/contest/biweekly-contest-62/problems/convert-1d-array-into-2d-array/)
 
 //Modulo Approach//
-
-
 
 
 class Solution {
