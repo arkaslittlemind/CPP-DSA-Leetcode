02836464c31b8226bc6ec36d4f20ebf943b01d9a
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-10-29 02:25:01 +0530

    Create 53. Maximum Subarray Optimal.cpp
    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
    
    A subarray is a contiguous part of an array.

---
+++ Arrays/53. Maximum Subarray Optimal.cpp
@@ -0,0 +1,17 @@
+Question Link- https://leetcode.com/problems/maximum-subarray/
+
+// Using Kadane's Algorithm//
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int maximum = INT_MIN;
+        for(auto it : nums){
+            sum += it;
+            maximum = max(sum, maximum);
+            if(sum < 0) sum = 0;
+        }
+        return maximum;
+    }
+};
