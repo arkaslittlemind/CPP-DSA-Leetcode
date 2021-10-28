2bddea7ce1bc520c4394b7137b5a198c14e1887b
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-10-28 21:56:01 +0530

    Create 1920. Build Array from Permutation.cpp
    Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
    
    A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).

---
+++ Arrays/1920. Build Array from Permutation.cpp
@@ -0,0 +1,15 @@
+Question Link- https://leetcode.com/problems/build-array-from-permutation/
+
+// Brute Force Approach//
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n= nums.size();
+       vector<int> ans;
+        for(int i=0; i<n; i++)
+            ans.push_back(nums[nums[i]]);
+         return ans;
+    }
+   
+};
