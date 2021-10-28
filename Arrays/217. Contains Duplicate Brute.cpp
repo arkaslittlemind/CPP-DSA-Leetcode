64b5a2394ba539cc6eda1c0cdab3283d659a67a7
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-10-29 01:30:26 +0530

    Create 217. Contains Duplicate Brute.cpp
    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.

---
+++ Arrays/217. Contains Duplicate Brute.cpp
@@ -0,0 +1,15 @@
+Question Link- https://leetcode.com/problems/contains-duplicate/
+
+// Brute Force Approach//
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i-1]==nums[i])
+                return true;
+        }
+       return false;
+    }
+};
