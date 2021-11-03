59e99d3d8ba61e78527c5894849d40537b617cda
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-03 23:50:34 +0530

    Create 27. Remove Element.cpp
    Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
    
    Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
    
    Return k after placing the final result in the first k slots of nums.
    
    Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
    
    Custom Judge:
    
    The judge will test your solution with the following code:
    
    int[] nums = [...]; // Input array
    int val = ...; // Value to remove
    int[] expectedNums = [...]; // The expected answer with correct length.
                                // It is sorted with no values equaling val.
    
    int k = removeElement(nums, val); // Calls your implementation
    
    assert k == expectedNums.length;
    sort(nums, 0, k); // Sort the first k elements of nums
    for (int i = 0; i < actualLength; i++) {
        assert nums[i] == expectedNums[i];
    }
    If all assertions pass, then your solution will be accepted.

---
+++ Arrays/27. Remove Element.cpp
@@ -0,0 +1,21 @@
+Question Link- https://leetcode.com/problems/remove-element/
+
+//Brute Force Approach//
+
+class Solution {
+public:
+	int removeElement(vector<int>& nums, int val) {
+		int length = nums.size(), index = 0;
+		while ( index < length )
+		{
+			 if ( nums[index] == val )
+			 {
+				 nums[index] = nums[length - 1];
+				 -- length;
+			 }
+				
+			else ++index;
+		}
+		 return length;  
+	}
+};
