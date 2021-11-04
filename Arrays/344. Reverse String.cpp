cbe979044c3e76903ba786e9a8160e2c42df000f
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-04 14:37:52 +0530

    Create 344. Reverse String.cpp
    Write a function that reverses a string. The input string is given as an array of characters s.

---
+++ Arrays/344. Reverse String.cpp
@@ -0,0 +1,10 @@
+Question Link- https://leetcode.com/problems/reverse-string/
+
+//Brute Force Approach(One Liner)//
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       reverse(s.begin(), s.end());
+    }
+};
