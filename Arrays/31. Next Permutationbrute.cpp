701e6c7e499cd95c5e8b221dc3975e2a3ed2dd44
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-07 14:55:36 +0530

    Create 31. Next Permutationbrute.cpp
    Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
    
    If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
    
    The replacement must be in place and use only constant extra memory.

---
+++ Arrays/31. Next Permutationbrute.cpp
@@ -0,0 +1,9 @@
+Question Link- https://leetcode.com/problems/next-permutation/
+
+//Brute Force Approach using C++ STL//
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+     next_permutation(nums.begin(), nums.end());
+};
