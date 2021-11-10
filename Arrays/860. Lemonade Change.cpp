696e451f43c88ba414727239a9fed32e94c50b46
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-11 00:01:06 +0530

    Update 860. Lemonade Change.cpp
    At a lemonade stand, each lemonade costs $5. Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills). Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill. You must provide the correct change to each customer so that the net transaction is that the customer pays $5.
    
    Note that you don't have any change in hand at first.
    
    Given an integer array bills where bills[i] is the bill the ith customer pays, return true if you can provide every customer with correct change, or false otherwise.

--- Arrays/860. Lemonade Change.cpp
+++ Arrays/860. Lemonade Change.cpp
@@ -1,1 +1,21 @@
 Question Link- https://leetcode.com/problems/lemonade-change/
+
+//Brute Force Approach//
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int m=0, n=0;
+        for(auto &i:bills){
+            switch(i){
+                case 5: m++; break;
+                case 10: m--, n++; break;
+                default: if(n) n--, m--;
+                    else m-=3;
+            }
+             if(m < 0 || n < 0) return false;
+                    
+            }
+       return true;  
+    }
+};
