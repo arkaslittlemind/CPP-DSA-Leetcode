8f59f121b2afdcd8527a7f99cf369ad4ba50be03
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-16 00:02:04 +0530

    Update 2075. Decode the Slanted Ciphertext.cpp

--- Weekly Contest 267/2075. Decode the Slanted Ciphertext.cpp
+++ Weekly Contest 267/2075. Decode the Slanted Ciphertext.cpp
@@ -1,3 +1,20 @@
 Question Link- https://leetcode.com/contest/weekly-contest-267/problems/decode-the-slanted-ciphertext/
 
+//Brute Force Approach//
 
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int n= encodedText.length();
+        int col = n/rows;
+        string res;
+        for(int i=0;i<col;i++){
+            for(int j=i; j<n; j+=(col+1)){
+                res += encodedText[j];
+            }
+        }
+        while(res.back() == ' ') res.pop_back();
+        return res;
+        
+    }
+};
