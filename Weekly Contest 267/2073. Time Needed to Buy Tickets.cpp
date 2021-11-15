1b36418c9c6cf686addcfd7bdf6eb94c880fed55
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-15 23:43:27 +0530

    Update 2073. Time Needed to Buy Tickets.cpp

--- Weekly Contest 267/2073. Time Needed to Buy Tickets.cpp
+++ Weekly Contest 267/2073. Time Needed to Buy Tickets.cpp
@@ -1,1 +1,15 @@
 Question Link- https://leetcode.com/contest/weekly-contest-267/problems/time-needed-to-buy-tickets/
+
+//Brute Force Approach//
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& a, int k) {
+        int tm=0;
+        for(int i=0; i<a.size();i++){
+            if(i <= k) tm += min(a[i], a[k]);
+            else tm += min(a[i], a[k]-1);
+        }
+        return tm;
+    }
+};
