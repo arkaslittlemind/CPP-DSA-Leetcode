cbde76133c1913f3c84519080a33e8621386415d
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-18 13:27:06 +0530

    Create 225. Implement Stack using Queues using a single queue.cpp
    Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
    
    Implement the MyStack class:
    
    void push(int x) Pushes element x to the top of the stack.
    int pop() Removes the element on the top of the stack and returns it.
    int top() Returns the element on the top of the stack.
    boolean empty() Returns true if the stack is empty, false otherwise.
    Notes:
    
    You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
    Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.

---
+++ Stack/225. Implement Stack using Queues using a single queue.cpp
@@ -0,0 +1,42 @@
+Question Link- https://leetcode.com/problems/implement-stack-using-queues/
+
+//Using a single queue//
+
+class MyStack {
+    queue<int> q; //using a single queue//
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i=0; i<q.size()-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int temp=q.front();
+        q.pop();
+        return temp;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
