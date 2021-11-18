c5942e4fed3a4d8246848c8d6d1d589df0eb3084
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-18 13:21:54 +0530

    Update 225. Implement Stack using Queues.cpp

--- Stack/225. Implement Stack using Queues.cpp
+++ Stack/225. Implement Stack using Queues.cpp
@@ -1,2 +1,45 @@
 Question Link- https://leetcode.com/problems/implement-stack-using-queues/
 
+//Using 2 Queues//
+
+class MyStack {
+    queue<int> q1;
+    queue<int> q2;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q2.push(x);
+        
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1,q2);
+    }
+    
+    int pop() {
+        int temp = q1.front();
+        q1.pop();
+        return temp;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
