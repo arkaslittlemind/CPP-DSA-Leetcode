624f81a86bb84483a35bbed35aeb53f8f4800f6d
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-22 20:36:22 +0530

    Update 191. Number of 1 Bits.cpp

--- Math/191. Number of 1 Bits.cpp
+++ Math/191. Number of 1 Bits.cpp
@@ -2,3 +2,16 @@ Question Link- https://leetcode.com/problems/number-of-1-bits/
 
 //Brute Force Approach//
 
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+       int count = 0;
+        while(n!=0){
+            if(n&1){
+                count++;
+            }
+             n = n>>1;
+        }
+        return count;
+    }
+};
