d2623a227bef9f5103c7fdfb0053ad11bb4fb0a6
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-24 21:59:07 +0530

    Update 1. Two Sum Brute.cpp

--- Arrays/1. Two Sum Brute.cpp
+++ Arrays/1. Two Sum Brute.cpp
@@ -5,17 +5,17 @@ Question Link- https://leetcode.com/problems/two-sum/
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
+        vector<int> result;
         for(int i=0; i<nums.size(); i++){
             for(int j=i+1; j<nums.size(); j++){
                 if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
+                    result.push_back(i);
+                    result.push_back(j);
                     break;
                 }
                     
             }
         }
-        return ans;
+        return result;
     }
 };
