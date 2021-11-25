266446725604937ba732b48b9640e45eeddd0b00
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-26 00:40:05 +0530

    Create 84. Largest Rectangle in Histogram1.cpp
    Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.

---
+++ Stack/84. Largest Rectangle in Histogram1.cpp
@@ -0,0 +1,25 @@
+Question Link- https://leetcode.com/problems/largest-rectangle-in-histogram/
+
+//Using Monotonic Stack//
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int maxArea = 0;
+        stack<int> st;
+        int n = heights.size();
+        for(int i = 0; i<=n; i++){
+            while(!st.empty() && (i==n || heights[st.top()] >= heights[i])){
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = i -st.top() - 1;
+                
+                maxArea = max(maxArea, width * height);
+            }
+            st.push(i);
+        }
+        return maxArea;
+    }
+};
