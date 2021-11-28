2f40de4ce59208120a4403f82d76a4620e4ce197
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-29 00:25:27 +0530

    Update 322. Coin Change.cpp

--- Arrays/322. Coin Change.cpp
+++ Arrays/322. Coin Change.cpp
@@ -2,3 +2,11 @@ Question Link- https://leetcode.com/problems/coin-change/
 
 //Brute Force Approach//
 
+vector<int> v(amount+1,amount+1);
+        v[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(auto c:coins){
+                if(i>=c) v[i]=min(v[i],v[i-c]+1);
+            }
+        }
+        return v.back()>amount ? -1 : v.back(); 
