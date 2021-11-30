7ae3ce55c59408f9992c7cd2c99c556ac6f73e2c
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-11-30 12:12:08 +0530

    Create 2085. Count Common Words With One Occurrence.cpp
    Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.

---
+++ Strings/2085. Count Common Words With One Occurrence.cpp
@@ -0,0 +1,21 @@
+Question Link- https://leetcode.com/contest/biweekly-contest-66/problems/count-common-words-with-one-occurrence/
+
+//Brute Force using Two Maps//
+
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        map<string, int> mp1, mp2;
+        for(auto &it: words1) mp1[it]++;
+        for(auto &it: words2) mp2[it]++;
+        
+        int count = 0;
+        for(auto it: mp1){
+            if(it.second == 1){
+                if(mp2[it.first] == 1) count++;
+        }
+    }
+    return count;
+    }
+};
+
