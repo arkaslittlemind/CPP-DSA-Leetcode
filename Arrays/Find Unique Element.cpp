1f2236d8298c5bdef22be399819e3eed6486d0f6
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-12-04 21:30:15 +0530

    Create Find Unique Element.cpp
    Problem Statement
    You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
    Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
    You need to find and return that number which is unique in the array/list.

---
+++ Arrays/Find Unique Element.cpp
@@ -0,0 +1,14 @@
+Question Link- https://www.codingninjas.com/codestudio/problems/find-unique_625159?leftPanelTab=0
+
+//Brute Force Approach//
+
+int findUnique(int *arr, int size)
+{
+    //Write your code here
+    int flag = 0;
+    
+    for(int i = 0; i<size; i++){
+       flag = arr[i] ^ flag;
+    }
+    return flag;
+}
