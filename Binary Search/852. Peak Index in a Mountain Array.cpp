92d62dd7b4a88a73920fe11bd160ee9e6e494690
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-12-07 23:51:28 +0530

    Update 852. Peak Index in a Mountain Array.cpp

--- Binary Search/852. Peak Index in a Mountain Array.cpp
+++ Binary Search/852. Peak Index in a Mountain Array.cpp
@@ -2,3 +2,26 @@ Question Link- https://leetcode.com/problems/peak-index-in-a-mountain-array/
 
 //Using Binary Search//
 
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int start = 0;
+        int end = n - 1;
+        
+        int mid = start + (end - start) / 2;
+        
+        while(start < end){
+            if(arr[mid] < arr[mid+1])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid;
+            }
+            mid = start + (end - start) / 2;
+        }
+        return start;
+    }
+};
