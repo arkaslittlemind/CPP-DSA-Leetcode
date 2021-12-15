f0039fae3e5df8df7feaec9ba3d66454a29ca38f
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-12-15 10:58:04 +0530

    Create Day 15 Insertion Sort List.cpp
    Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
    
    The steps of the insertion sort algorithm:
    
    Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
    At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
    It repeats until no input elements remain.
    The following is a graphical example of the insertion sort algorithm. The partially sorted list (black) initially contains only the first element in the list. One element (red) is removed from the input data and inserted in-place into the sorted list with each iteration.

---
+++ December Daily Challenge/Day 15 Insertion Sort List.cpp
@@ -0,0 +1,50 @@
+Question Link- https://leetcode.com/problems/insertion-sort-list/
+
+//Using Insertion Sort and Pointers//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+         if (!head) return NULL;
+        ListNode *tail = head, *curr, *iter;
+        while (tail->next) {
+            // checking if the next one is already the next bigger
+            if (tail->val <= tail->next->val) {
+                tail = tail->next;
+                continue;
+            }
+            // taking a new node to parse, curr, out of the list
+            curr = tail->next;
+            tail->next = curr->next;
+            // checking if curr will become the new head
+            if (curr->val < head->val) {
+                curr->next = head;
+                head = curr;
+                continue;
+            }
+            // all the other cases
+            iter = head;
+            while (iter != tail) {
+                // checking when we can splice curr between iter and the following value
+                if (curr->val < iter->next->val) {
+                    curr->next = iter->next;
+                    iter->next = curr;
+                    break;
+                }
+                // moving to the next!
+                iter = iter->next;
+            }
+        }
+        return head;
+    }
+};
