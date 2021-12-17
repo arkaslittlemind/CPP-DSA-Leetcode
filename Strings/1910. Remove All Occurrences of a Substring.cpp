2c168d1aaae16c6bafbeb6a4351d813618bfcea0
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-12-17 22:50:33 +0530

    Update 1910. Remove All Occurrences of a Substring.cpp

--- Strings/1910. Remove All Occurrences of a Substring.cpp
+++ Strings/1910. Remove All Occurrences of a Substring.cpp
@@ -1,2 +1,15 @@
 Question Link- https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
 
+// Using CPP In-built functions find(), erase() //
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int n = s.length();
+        
+        while(n != 0 && s.find(part) < n) {
+            s.erase(s.find(part), part.length());
+        }
+        return s;
+    }
+};
