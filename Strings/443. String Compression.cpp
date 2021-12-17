4ef0cb72c1b4fc5aba52d601148f209defa61493
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-12-17 23:45:09 +0530

    Update 443. String Compression.cpp

--- Strings/443. String Compression.cpp
+++ Strings/443. String Compression.cpp
@@ -1,1 +1,31 @@
 Question Link- https://leetcode.com/problems/string-compression/
+
+
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+      int n = chars.size();
+      int i = 0;
+      int ansIndex = 0; 
+        
+        while(i < n) {
+            int j = i + 1;
+            while(j < n && chars[i] == chars[j]) {
+                j++;
+            }
+            
+            chars[ansIndex++] = chars[i];
+            int count = j - 1;
+            
+            if(count > 1) {
+                string cnt = to_string(count);
+                for(char ch: cnt) {
+                   chars[ansIndex++] = ch; 
+                }
+            }
+            i = j;
+        }
+        return ansIndex;
+    }
+};
