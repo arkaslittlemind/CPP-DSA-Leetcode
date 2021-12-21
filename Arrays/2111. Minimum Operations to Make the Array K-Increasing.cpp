34460fb5c4d914a2dcf2d1a84bb17669448bd28d
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-12-21 22:52:43 +0530

    Update 2111. Minimum Operations to Make the Array K-Increasing.cpp
    You are given a 0-indexed array arr consisting of n positive integers, and a positive integer k.
    
    The array arr is called K-increasing if arr[i-k] <= arr[i] holds for every index i, where k <= i <= n-1.
    
    For example, arr = [4, 1, 5, 2, 6, 2] is K-increasing for k = 2 because:
    arr[0] <= arr[2] (4 <= 5)
    arr[1] <= arr[3] (1 <= 2)
    arr[2] <= arr[4] (5 <= 6)
    arr[3] <= arr[5] (2 <= 2)
    However, the same arr is not K-increasing for k = 1 (because arr[0] > arr[1]) or k = 3 (because arr[0] > arr[3]).
    In one operation, you can choose an index i and change arr[i] into any positive integer.
    
    Return the minimum number of operations required to make the array K-increasing for the given k.

--- Arrays/2111. Minimum Operations to Make the Array K-Increasing.cpp
+++ Arrays/2111. Minimum Operations to Make the Array K-Increasing.cpp
@@ -1,2 +1,35 @@
 Question Link- https://leetcode.com/problems/minimum-operations-to-make-the-array-k-increasing/
 
+// Binary Search (Longest Non Decreasing Subsequence) //
+
+class Solution {
+private:
+    int lnds(vector<int> &arr) {
+        vector<int> temp;
+        int n = arr.size();
+        for(int i = 0; i < n; i++) {
+            if(temp.size() == 0 || temp.back() <= arr[i]) {
+                temp.push_back(arr[i]);
+            }
+            else {
+                int ind = upper_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+                temp[ind] = arr[i];
+            }
+        }
+        return temp.size();
+    }
+    
+public:
+    int kIncreasing(vector<int>& arr, int k) {
+        int n = arr.size();
+        int minchanges = 0;
+        for(int i = 0; i < k; i++) {
+            vector<int> sbarr;
+            for(int j = i; j < n; j += k) {
+                sbarr.push_back(arr[j]);
+            }
+            minchanges += sbarr.size() - lnds(sbarr);
+        }
+        return minchanges;
+    }
+};
