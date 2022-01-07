6e78ce508792d25f6fe03da1a051f8e06139d86b
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-07 20:41:31 +0530

    Create Day 7 Linked List Random Node.cpp
    Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
    
    Implement the Solution class:
    
        Solution(ListNode head) Initializes the object with the integer array nums.
        int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be choosen.

---
+++ January Daily Challenge/Day 7 Linked List Random Node.cpp
@@ -0,0 +1,23 @@
+Question Link- https://leetcode.com/problems/linked-list-random-node/
+
+class Solution {
+public:
+    //Note : head is guaranteed to be not null, so it contains at least one node.
+    ListNode* HeadNode;
+    Solution(ListNode* head) {
+       HeadNode = head;
+    }
+    //returns value of a random node
+    int getRandom() {
+        int res, len = 1;
+        ListNode* x = HeadNode;
+        while(x){
+            if(rand() % len == 0){
+                res = x->val;
+            }
+            len++;
+            x = x->next;
+        }
+        return res;
+    }
+};
