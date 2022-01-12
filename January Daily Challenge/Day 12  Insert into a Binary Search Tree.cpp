09ca7540220dee88a3c5103083b7c135b9bd34e7
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-12 14:59:10 +0530

    Create Day 12  Insert into a Binary Search Tree.cpp
    You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
    
    Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.

---
+++ January Daily Challenge/Day 12  Insert into a Binary Search Tree.cpp
@@ -0,0 +1,11 @@
+Question Link- https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(!root) return new TreeNode(val);
+        if(root->val > val) root->left = insertIntoBST(root->left, val);
+        else root->right = insertIntoBST(root->right, val);
+        return root;
+    }
+};
