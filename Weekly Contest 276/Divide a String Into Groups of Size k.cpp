682309d2a626041783e74b251e998ba212943b1b
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-16 22:19:57 +0530

    Update and rename 1.cpp to Divide a String Into Groups of Size k.cpp
    A string s can be partitioned into groups of size k using the following procedure:
    
        The first group consists of the first k characters of the string, the second group consists of the next k characters of the string, and so on. Each character can be a part of exactly one group.
        For the last group, if the string does not have k characters remaining, a character fill is used to complete the group.
    
    Note that the partition is done so that after removing the fill character from the last group (if it exists) and concatenating all the groups in order, the resultant string should be s.
    
    Given the string s, the size of each group k and the character fill, return a string array denoting the composition of every group s has been divided into, using the above procedure.

---
+++ Weekly Contest 276/Divide a String Into Groups of Size k.cpp
@@ -0,0 +1,24 @@
+Question Link- https://leetcode.com/contest/weekly-contest-276/problems/divide-a-string-into-groups-of-size-k/
+
+class Solution {
+public:
+    vector<string> divideString(string s, int k, char fill) {
+        int n = s.size();
+        vector<string> ans;
+        string res = "";
+        for(int i = 0; i < n; i++){
+            res += s[i];
+            if(res.size() == k){
+                ans.push_back(res);
+                res = "";
+            }
+        }
+        if(res.size() > 0 && res.size() != k){
+            while(res.size() != k){
+                res += fill;
+            }
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
