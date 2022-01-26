472f759deb9ffcb1ac1acd8272aca168a2e5cc23
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-26 22:15:21 +0530

    Create Day 26 All Elements in Two Binary Search Trees.cpp
    Given two binary search trees root1 and root2, return a list containing all the integers from both trees sorted in ascending order.

---
+++ January Daily Challenge/Day 26 All Elements in Two Binary Search Trees.cpp
@@ -0,0 +1,22 @@
+Question Link- https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+
+class Solution
+{
+public:
+     vector<int> res;
+     void dfs(TreeNode *root)
+     {
+          if (!root)
+               return;
+          res.push_back(root->val);
+          dfs(root->left);
+          dfs(root->right);
+     }
+     vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+     {
+          dfs(root1);
+          dfs(root2);
+          sort(begin(res), end(res));
+          return res;
+     }
+};
