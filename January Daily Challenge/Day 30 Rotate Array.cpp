dd980af0174f15475bda5964bf4c552bd7369e76
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-30 20:14:20 +0530

    Create Day 30 Rotate Array.cpp
    Given an array, rotate the array to the right by k steps, where k is non-negative.

---
+++ January Daily Challenge/Day 30 Rotate Array.cpp
@@ -0,0 +1,14 @@
+Question Link- https://leetcode.com/problems/rotate-array/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans(n);
+        
+        for(int i = 0; i < n; i++){
+            ans[(i+k) % n] = nums[i];
+        }
+        nums = ans;
+    }
+};
