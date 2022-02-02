2e59abf7a3294a3a6c8387d011d3a0a5dd39ed34
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-02-02 22:17:46 +0530

    Create Day 2 Find All Anagrams in a String.cpp
    Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
    
    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.

---
+++ February Daily Challenge/Day 2 Find All Anagrams in a String.cpp
@@ -0,0 +1,40 @@
+Question Link- https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> res;
+        unordered_map<char,int> m;
+        for(int i=0;i<p.length();i++)
+            m[p[i]]++;
+        int count=m.size();
+        int k=p.length();//Window size.
+        int i=0;
+        int j=0;
+        while(j<s.length())
+        {
+            if(m.find(s[j])!=m.end())
+            {
+                m[s[j]]--;
+                if(m[s[j]]==0)
+                    count--;
+            }
+            if(j-i+1<k)
+                j++;
+            else if(j-i+1==k)
+            {
+                if(count==0)
+                    res.push_back(i);
+                if(m.find(s[i])!=m.end())
+                {
+                    m[s[i]]++;
+                    if(m[s[i]]==1)
+                        count++;
+                }
+                i++;
+                j++;
+            }
+        }
+        return res;
+    }
+};
