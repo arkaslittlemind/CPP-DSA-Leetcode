b91dcd57fa9554b91145304d0dbd8b0ce11f60bb
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-02-07 21:52:47 +0530

    Create Day 7 Find the Difference.cpp
    You are given two strings s and t.
    
    String t is generated by random shuffling string s and then add one more letter at a random position.
    
    Return the letter that was added to t.

---
+++ February Daily Challenge/Day 7 Find the Difference.cpp
@@ -0,0 +1,11 @@
+Question Link- https://leetcode.com/problems/find-the-difference/
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char v = 0;
+        for (char c: s) v ^= c;
+        for (char c: t) v ^= c;
+        return v;
+    }
+};
