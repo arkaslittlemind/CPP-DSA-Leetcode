3812101eee66984dd062f4a57ac0c8a6f263cb19
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-02-15 21:52:36 +0530

    Create Day 15 Single Number.cpp
    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
    You must implement a solution with a linear runtime complexity and use only constant extra space.

---
+++ February Daily Challenge/Day 15 Single Number.cpp
@@ -0,0 +1,13 @@
+Question Link- https://leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
