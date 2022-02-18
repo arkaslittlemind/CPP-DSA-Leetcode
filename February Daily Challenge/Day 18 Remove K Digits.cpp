178df642de3d049abb15039068955cabc5498c19
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-02-18 22:04:19 +0530

    Create Day 18 Remove K Digits.cpp
    Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.

---
+++ February Daily Challenge/Day 18 Remove K Digits.cpp
@@ -0,0 +1,22 @@
+Question Link- https://leetcode.com/problems/remove-k-digits/
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+         string ans="";
+        for(char &c:num)
+        {
+            while(ans.size() && ans.back()>c &&k)
+            {
+                ans.pop_back();
+                k--;
+            }
+            if(ans.size()||c!='0')ans.push_back(c);
+        }
+        while(ans.size()&&k--)          
+        {
+            ans.pop_back();
+        }
+        return (ans=="")?"0":ans;
+    }
+};
