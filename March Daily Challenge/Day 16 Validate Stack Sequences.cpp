c15c1b61fe6b10e0b2f3108d5dab0102b40dd665
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-03-16 20:12:14 +0530

    Create Day 16 Validate Stack Sequences.cpp
    Given two integer arrays pushed and popped each with distinct values, return true if this could have been the result of a sequence of push and pop operations on an initially empty stack, or false otherwise.

---
+++ March Daily Challenge/Day 16 Validate Stack Sequences.cpp
@@ -0,0 +1,17 @@
+Question Link- https://leetcode.com/problems/validate-stack-sequences/
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> stack;
+        int i = 0;
+        for (int x : pushed) {
+            stack.push(x);
+            while (stack.size() && stack.top() == popped[i]) {
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.size() == 0;
+    }
+};
