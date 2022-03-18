65416d9864a2f52520fb892afcc98fdd5f542113
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-03-18 19:48:46 +0530

    Create Day 17 Score of Parantheses.cpp
    Given a balanced parentheses string s, return the score of the string.
    
    The score of a balanced parentheses string is based on the following rule:
    
        "()" has score 1.
        AB has score A + B, where A and B are balanced parentheses strings.
        (A) has score 2 * A, where A is a balanced parentheses string.

---
+++ March Daily Challenge/Day 17 Score of Parantheses.cpp
@@ -0,0 +1,20 @@
+Question Link- https://leetcode.com/problems/score-of-parentheses/
+
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+         stack<int> st;
+        int score = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                st.push(score);
+                score = 0;
+            }
+            else {
+                score = st.top() + max(2 * score, 1);
+                st.pop();
+            }
+        }
+        return score;
+    }
+};
