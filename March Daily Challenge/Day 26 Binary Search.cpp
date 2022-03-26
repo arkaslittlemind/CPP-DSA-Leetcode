55b99653cee8bb24c6898657475173f3128d252d
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-03-26 11:17:41 +0530

    Create Day 26 Binary Search.cpp
    Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
    
    You must write an algorithm with O(log n) runtime complexity.

---
+++ March Daily Challenge/Day 26 Binary Search.cpp
@@ -0,0 +1,16 @@
+Question Link- https://leetcode.com/problems/binary-search/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+         int n = nums.size()-1;
+        int low = 0, high = n;
+        while( low <= high){
+            int mid = low + (high-low)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) high = mid -1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+};
