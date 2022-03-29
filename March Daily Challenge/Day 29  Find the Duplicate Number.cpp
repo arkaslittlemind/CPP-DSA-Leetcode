fdbcb857acb30a71b9df18995521e60c17356283
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-03-29 20:23:36 +0530

    Create Day 29  Find the Duplicate Number.cpp
    Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
    
    There is only one repeated number in nums, return this repeated number.
    
    You must solve the problem without modifying the array nums and uses only constant extra space.

---
+++ March Daily Challenge/Day 29  Find the Duplicate Number.cpp
@@ -0,0 +1,19 @@
+Question Link- https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n+1, 0);
+        for(int i = 0; i<n; i++){
+            if(ans[nums[i]]!=0){
+                return nums[i];
+            }
+            else{
+                ans[nums[i]]++;
+            }
+                
+        }
+        return -1;
+    }
+};
