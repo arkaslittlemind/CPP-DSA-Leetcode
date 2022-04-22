b268468d2f83450d2bacbcdecf0d6fe042e39930
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-04-22 13:34:23 +0530

    Create Day 14 Search in a Binary Tree.cpp
    You are given the root of a binary search tree (BST) and an integer val.
    
    Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.

---
+++ April Daily Challenge/Day 14 Search in a Binary Tree.cpp
@@ -0,0 +1,15 @@
+Question Link- https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL)
+            return NULL;
+        if(root->val==val)
+            return root;
+        if(root->val<val)
+            return searchBST(root->right,val);
+        else
+            return searchBST(root->left,val);
+    }
+};
