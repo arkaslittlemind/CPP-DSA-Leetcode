2c4dc66fb8cc11af1551e51af451b64febd850c0
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-04-22 13:45:21 +0530

    Create Day 18 Kth Smallest Element in a BST.cpp
    Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.

---
+++ April Daily Challenge/Day 18 Kth Smallest Element in a BST.cpp
@@ -0,0 +1,17 @@
+Question Link- https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+class Solution {
+public:
+    vector<int> a;
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root);
+        int ans = a[k-1];
+        return ans;
+    }
+    void inorder(TreeNode* root){
+        if(!root) return;
+        inorder(root->left);
+        a.push_back(root->val);
+        inorder(root->right);
+    }
+};
