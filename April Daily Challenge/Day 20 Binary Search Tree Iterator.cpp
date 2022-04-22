61b3f7db67181740dc65a080c67e15f94e887e35
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-04-22 13:48:43 +0530

    Create Day 20 Binary Search Tree Iterator.cpp
    Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
    
        BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
        boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
        int next() Moves the pointer to the right, then returns the number at the pointer.
    
    Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
    
    You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.

---
+++ April Daily Challenge/Day 20 Binary Search Tree Iterator.cpp
@@ -0,0 +1,27 @@
+Question Link- https://leetcode.com/problems/binary-search-tree-iterator/
+
+class BSTIterator {
+public:
+    stack<TreeNode*> s;
+    BSTIterator(TreeNode* root) {
+        partialInorder(root);
+    }
+    
+    void partialInorder(TreeNode* root){
+        while(root != NULL){
+            s.push(root);
+            root = root->left;
+        }
+    }
+    
+    int next() {
+        TreeNode* top = s.top();
+        s.pop();
+        partialInorder(top->right);
+        return top->val;
+    }
+    
+    bool hasNext() {
+        return !s.empty();
+    }
+};
