77802ea93111ab62726529afcd954edc5c50e6d9
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-04-22 18:06:55 +0530

    Create Day 21 Design HashSet.cpp
    Design a HashSet without using any built-in hash table libraries.
    
    Implement MyHashSet class:
    
        void add(key) Inserts the value key into the HashSet.
        bool contains(key) Returns whether the value key exists in the HashSet or not.
        void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.

---
+++ April Daily Challenge/Day 21 Design HashSet.cpp
@@ -0,0 +1,20 @@
+Question Link- https://leetcode.com/problems/design-hashset/
+
+class MyHashSet {
+private:
+	vector<bool> table;
+public:
+	MyHashSet() : table(1e6 + 1, false) {}
+	
+	void add(int key) {
+		table[key] = true;
+	}
+	
+	void remove(int key) {
+		table[key] = false;
+	}
+	
+	bool contains(int key) {
+		return table[key];
+	}
+};
