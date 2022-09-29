3012859b8d853f1c3273f030541f158f866419d2
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-09-29 11:11:13 +0530

    Create 876. Middle of the Linked List.cpp
    Given the head of a singly linked list, return the middle node of the linked list.
    
    If there are two middle nodes, return the second middle node.

---
+++ Linked List/876. Middle of the Linked List.cpp
@@ -0,0 +1,31 @@
+Question Link- https://leetcode.com/problems/middle-of-the-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int length = 0;
+        ListNode* temp = head;
+        
+        while(temp != NULL){
+            length++;
+            temp = temp -> next;
+        }
+        int midPoint = length / 2;
+        temp = head;
+        
+        while(midPoint--){
+            temp = temp -> next; 
+        }
+        return temp;
+    }
+};
