9d39e89e924021dadb60b5c73e1dbe39fb651d12
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-09-29 12:02:28 +0530

    Update 876. Middle of the Linked List.cpp

--- Linked List/876. Middle of the Linked List.cpp
+++ Linked List/876. Middle of the Linked List.cpp
@@ -1,5 +1,9 @@
 Question Link- https://leetcode.com/problems/middle-of-the-linked-list/
 
+Time Complexity: O(N) + O(N/2) (N: length of linked list)
+
+Space Complexity: O(1) since two variables are being used
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -27,5 +31,35 @@ public:
             temp = temp -> next; 
         }
         return temp;
+    }
+};
+
+
+------------------------------------------------------------------------------
+Optimised Approach- Slow & Fast Pointer Approach
+
+Time Complexity: O(N)
+
+Space Complexity: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+       ListNode *slow = head, *fast = head;
+        while(fast != NULL && fast -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        return slow;
     }
 };
