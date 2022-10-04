b03710aedc137ac785a0ec4ca715f032ff0390a1
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-10-04 22:29:59 +0530

    Update 705. Design HashSet.cpp

--- Linked List/705. Design HashSet.cpp
+++ Linked List/705. Design HashSet.cpp
@@ -33,4 +33,59 @@ public:
  */
 
 ---------------------------------------------------------------------------------------------------
+    
+//Using Double Linked List //
+    
+class MyHashSet {
+public:
+    vector<list<int>> m;
+    int size;
+    MyHashSet() {
+        size = 100;
+        m.resize(size);
+    }
+    
+    //Hashing Function
+    int hash(int key){
+        return key % size;
+    }
+    
+    
+    //Search Function
+    list <int> ::iterator search(int key){
+        int i = hash(key);
+        return find(m[i].begin(), m[i].end(), key);
+    }
+    
+    
+    
+    void add(int key) {
+        if(contains(key))
+            return;
+        int  i = hash(key);
+        m[i].push_back(key);
+    }
+    
+    void remove(int key) {
+        if(!contains(key)) return;
+        int i = hash(key);
+        m[i].erase(search(key));
+    }
+    
+    bool contains(int key) {
+        int i = hash(key);
+        if(search(key) != m[i].end()) 
+            return true;
+        else
+            return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
   
