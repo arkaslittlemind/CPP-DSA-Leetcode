1f37afc7303af548545e66d9fd3f509f086dca74
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-10-08 22:52:34 +0530

    Time: 27 ms (16.83%), Space: 11.7 MB (7.97%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -16,7 +16,7 @@ public:
         while(curr != NULL && curr -> next != NULL){
             if(curr -> val == curr -> next -> val){
                 ListNode *temp = curr -> next;
-                curr -> next = curr -> next -> next;
+                curr -> next = temp -> next;
                 delete (temp);
             } else 
                 curr = curr -> next;
