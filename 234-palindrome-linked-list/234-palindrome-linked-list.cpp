2c7163a675e10f3e711b2ad84fb494bd06ae53f5
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-10-13 12:59:56 +0530

    Time: 536 ms (19.47%), Space: 114.2 MB (76.96%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -9,17 +9,67 @@
  * };
  */
 class Solution {
+    
+private:
+    
+    ListNode *reverseList(ListNode *head)
+{
+    if(head == NULL)
+        return NULL ;
+
+    // We need 3 pointers to Reverse the LinkedList - Previous , Current & Next
+
+    // Initally, Previous points to NULL
+
+    ListNode *p = NULL ;
+
+    // Current points to Head
+
+    ListNode *c = head ;
+
+    // Next point to Head's Next Node
+
+    ListNode *n = head -> next ;
+
+    // We use the same logic to reverse the linked list as we did in lecture 10
+
+    while(c != NULL)
+    {
+        c -> next = p ;
+
+        p = c ;
+        c = n ;
+
+        if(n != NULL)
+            n = n -> next ;
+    }
+
+    return p ;
+}
+    
 public:
+    
+    
     bool isPalindrome(ListNode* head) {
-       stack<int> st;
+        if(head == NULL) return true;
         
-        for(ListNode *curr = head; curr != NULL; curr = curr -> next){
-            st.push(curr -> val);
+        //finding middle
+        ListNode *slow = head, *fast = head;
+        while(fast -> next != NULL && fast -> next -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
-        for(ListNode *curr = head; curr != NULL; curr = curr -> next){
-            if(st.top() != curr -> val)
+        
+        //reverse the 2nd half of the linked list
+        ListNode *rev = reverseList(slow -> next);
+        
+        ListNode *curr = head;
+        while(rev != NULL){
+            if(rev -> val != curr -> val)
                 return false;
-            st.pop();
+            
+            rev = rev -> next;
+            curr = curr -> next;
         }
         return true;
     }
